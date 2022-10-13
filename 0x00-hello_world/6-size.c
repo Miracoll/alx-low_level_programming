1109fc0757717f2967a2249d88a39e6351f88e40
Miracle <obianomiracle@gmail.com>
2022-10-13 08:34:19 -0700

    Addint task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,7 +7,9 @@
 
 int main(void)
 {
-	printf("Size of a char: %d byte\n",sizeof(char));
-	printf("size of an int: %d byte\n",sizeof(int));
-	printf("Size of a float: %d byte\n",sizeof(float));
+	printf("Size of a char: %d byte\n", sizeof(char));
+	printf("size of an int: %d byte\n", sizeof(int));
+	printf("Size of a float: %d byte\n", sizeof(float));
+	printf("Size of a long int: %d byte\n", sizeof(long int));
+	printf("Size of a long long int:%d byte\n", sizeof(long long int));
 }
