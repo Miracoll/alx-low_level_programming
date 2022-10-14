ce94aeef8d7ebffba7727afd140adf20d05692e2
Miracle <obianomiracle@gmail.com>
2022-10-14 05:27:35 -0700

    Updating

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -13,7 +13,7 @@ int main(void)
 
 	for (lower = 97; lower <= 122; lower++)
 		putchar(lower);
-	for (upper = 65; upper <= 90; upper)
+	for (upper = 65; upper <= 90; upper++)
 		putchar(upper);
 	putchar('\n');
 
