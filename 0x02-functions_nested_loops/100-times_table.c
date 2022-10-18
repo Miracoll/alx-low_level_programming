74ecc1b8f121e028c988325e0af27e1357ffd33c
Miracle <obianomiracle@gmail.com>
2022-10-18 07:53:43 -0700

    Adding advance task

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints the n times table
+ *
+ * Return: void
+ */
+
+void print_times_table(int n)
+{
+	int i, j, prod, tens, unit;
+
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j<= n; j++)
+		{
+			prod = i * j;
+			tens = prod / 10;
+			unit = prod % 10;
+
+			if (j == 0)
+				_putchar('0');
+			else if (prod < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(unit + 48);
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(unit + 48);
+				_putchar(unit + 48);
+			}
+		}
+		_putchar('\n');
+	}
+}
