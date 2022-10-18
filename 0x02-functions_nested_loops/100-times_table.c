b26bfc51293c7c06ee2727e4558019e0b56d0ad7
Miracle <obianomiracle@gmail.com>
2022-10-18 08:08:28 -0700

    Correction

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,16 +9,14 @@
 void print_times_table(int n)
 {
 	int i, j, prod;
-	
+
 	if (n >= 0 && n < 15)
 	{
 		for (i = 0; i <= n; i++)
 		{
-			for (j = 0; j<= n; j++)
+			for (j = 0; j <= n; j++)
 			{
 				prod = i * j;
-				tens = prod / 10;
-				unit = prod % 10;
 
 				if (j == 0)
 					_putchar('0');
@@ -27,14 +25,24 @@ void print_times_table(int n)
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
-					_putchar(unit + 48);
+					_putchar(' ');
+					_putchar(prod % 10 + 48);
 				}
-				else
+				else if (prod >= 10 && prod < 100)
 				{
 					_putchar(',');
 					_putchar(' ');
-					_putchar(unit + 48);
-					_putchar(unit + 48);
+					_putchar(' ');
+					_putchar(((prod / 10) % 10) + 48);
+					_putchar((prod % 10) + 48);
+				}
+				else if (prod > 99 && prod < 1000)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((prod / 100) + 48);
+					_putchar(((prod / 10) % 10) + 48);
+					_putchar((prod % 10) + 48);
 				}
 			}
 			_putchar('\n');
