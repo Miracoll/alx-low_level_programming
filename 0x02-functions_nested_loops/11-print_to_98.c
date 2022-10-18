c108d814427106e4b958e75360e0629860634412
Miracle <obianomiracle@gmail.com>
2022-10-18 07:16:12 -0700

    Adding task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,36 @@
+# include<stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - print numbers to 98
+ *
+ * @n: value to begin with
+ *
+ * Return: void
+ */
+
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		while (n <= 98)
+		{
+			printf("%d, ", n);
+			n++;
+		}
+		putchar('\n');
+	}
+	else if (n > 98)
+	{
+		while (n > 98)
+		{
+			printf("%d, ", n);
+			n--;
+		}
+		putchar('\n');
+	}
+	else
+	{
+		printf("98\n");
+	}
+}
