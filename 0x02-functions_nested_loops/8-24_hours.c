bf2d52a1420caf4d1661ee98d16af5473ed0919d
Miracle <obianomiracle@gmail.com>
2022-10-18 06:32:28 -0700

    Adding task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day
+ *
+ * Return: void
+ */
+
+void jack_bauer(void)
+{
+	int i, j, m, n;
+
+	for (i = 0; i <= 2; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			for (m = 0; n <= 5; n++)
+			{
+				for (n = 0; n <= 9; n++)
+				{
+					_putchar(i + 48);
+					_putchar(j + 48);
+					_putchar(58);
+					_putchar(m + 48);
+					_putchar(n + 48);
+					_putchar('\n');
+					if (i == 2 && j == 3 && m == 5 && n == 9)
+						break;
+				}
+				if (i == 2 && j == 3 && m == 5 && n == 9)
+					break;
+			}
+			if (i == 2 && j == 3 && m == 5 && n == 9)
+				break;
+		}
+		if (i == 2 && j == 3 && m == 5 && n == 9)
+			break;
+	}
+}
