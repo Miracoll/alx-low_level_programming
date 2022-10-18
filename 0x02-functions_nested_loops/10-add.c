ce7ff5bc85ab4fa290a4edf66d7386bdb6fe1e1d
Miracle <obianomiracle@gmail.com>
2022-10-18 07:04:15 -0700

    Adding task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * add - adds two integer values
+ *
+ * @n1: integer 1
+ *
+ * @n2: integer value 2
+ *
+ * Return: total
+ */
+
+int add(int n1, int n2)
+{
+	int total;
+
+	total = n1 + n2;
+	return (total);
+}
