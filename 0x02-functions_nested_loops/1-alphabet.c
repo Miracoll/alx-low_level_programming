97dc2b234f488b95c4de1804f6b6a698c4056870
Miracle <obianomiracle@gmail.com>
2022-10-18 01:45:13 -0700

    Adding task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int start;
+
+	for (start = 97; start <= 122; start++)
+		_puchar(start);
+	_putchar('\n');
+
+	return (0);
+}
