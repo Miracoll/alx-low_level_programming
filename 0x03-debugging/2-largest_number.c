9cffc4cb9bfc6999b5118d0310f55d944ceb5a1a
Miracle <obianomiracle@gmail.com>
2022-10-19 23:06:07 -0700

    Adding task 2

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * largest_number - return the lagest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
+ */
+
+int largest_number(int a, int b, int c)
+{
+	int largest;
+
+	if (a >= b && b > c)
+		largest = a;
+	else if (b >= a && a > c)
+		largest = b;
+	else if (c >= b && b > a)
+		largest = c;
+	else
+		largest = a;
+
+	return (largest);
+}
