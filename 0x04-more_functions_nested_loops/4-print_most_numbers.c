df8762104628ded106d0c3b240ab8c6b059434e5
Miracle <obianomiracle@gmail.com>
2022-10-20 15:44:20 -0700

    Add fix

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,11 +8,12 @@
 void print_most_numbers(void)
 {
 	int counter;
+
 	for (counter = 0; counter <= 9; counter++)
 	{
 		if (counter == 2 || counter == 4)
 			continue;
 		_putchar(counter);
 	}
-	_putchar('n');
+	_putchar('\n');
 }
