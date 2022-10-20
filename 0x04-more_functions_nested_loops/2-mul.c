f2928156d5c47e433320b5ac293e26b4e05fcdab
Miracle <obianomiracle@gmail.com>
2022-10-20 15:09:40 -0700

    Adding task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+int "main.h"
+
+/**
+ * mul - multiply two integer values
+ *
+ * @a: first integer value
+ *
+ * @b: second integer value
+ *
+ * Return: integer
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
