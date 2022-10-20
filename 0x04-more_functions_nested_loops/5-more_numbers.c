5c1215d2164f091c8a3a7fcf38e7bb22e752b190
Miracle <obianomiracle@gmail.com>
2022-10-20 15:57:27 -0700

    Adding task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * more_numbers - print 0 to 14 ten times
+ *
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int x, y;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y <= 14; y++)
+			_putchar(y + 48);
+		_putchar('\n');
+	}
+}
