39f374f57349e29c917310049210edb6da712645
Miracle <obianomiracle@gmail.com>
2022-10-20 15:39:56 -0700

    Adding task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "mmain.h"
+
+/**
+ * print_most_numbers - print number 0 to 9 skipping 2 and 4
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	int counter;
+	for (counter = 0; counter <= 9; counter++)
+	{
+		if (counter == 2 || counter == 4)
+			continue;
+		_putchar(counter);
+	}
+	_putchar('n');
+}
