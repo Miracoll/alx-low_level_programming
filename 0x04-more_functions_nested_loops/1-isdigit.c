31c47a8431203ad28b3591e823886ece87846c69
Miracle <obianomiracle@gmail.com>
2022-10-20 14:56:39 -0700

    Adding task 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main"
+
+/**
+ * _isdigit - checks for digithood
+ * @c: character to checked for numeric value
+ * Return 1 if numeric otherwise 0
+ */
+
+int _isdigit(int c)
+{
+	int number;
+	if (c >= '0' && c <= '9')
+		number = 1;
+	else
+		number = 0;
+}
