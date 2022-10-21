a0a88f54d35fd2a1a0307ae68f614a133372fd50
Miracle <obianomiracle@gmail.com>
2022-10-20 18:08:19 -0700

    Add

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - FizzBuzz challenge
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int counter;
+
+	for (counter = 0; counter < 100; counter++)
+	{
+		if (counter % 3 == 0 && counter % 5 != 0)
+			printf("Fizz");
+		else if (counter % 3 != 0 && counter % 5 == 0)
+			printf("Buzz");
+		else if (counteer % 3 == 0 && counter % 5 == 0)
+			printf("FizzBuzz");
+		else
+			printf("%d", counter);
+		if (counter != 100)
+			putchar(' ');
+	}
+	return (0);
+}
