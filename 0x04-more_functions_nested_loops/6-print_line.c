383e5bfd559e848882e8c18491438f2903114c66
Miracle <obianomiracle@gmail.com>
2022-10-20 17:07:21 -0700

    Adding task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - draw straight line in terminal
+ * Return: void
+ */
+void print_line(int n)
+{
+	int counter;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (counter = 0; counter <= n; counter++)
+			_putchar('_');
+		_putchar('\n');
+	}
+}
