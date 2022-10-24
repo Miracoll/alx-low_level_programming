b389aa11b95eb8710f0c21cebbe87e7b8697b0b9
Miracle <obianomiracle@gmail.com>
2022-10-24 07:18:36 -0700

    Adding task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts - output string
+ * @str: value to output
+ * Return: void
+ */
+
+void _put(char *str)
+{
+	int counter = 0;
+
+	while (str[counter] != '\0')
+	{
+		value = str[counter];
+		_putchar(value);
+		counter++;
+	}
+	_putchar('\n');
+}
