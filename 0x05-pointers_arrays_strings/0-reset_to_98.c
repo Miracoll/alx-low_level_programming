a14444d9f79adb9aae3bae74e48e011befcd694c
Miracle <obianomiracle@gmail.com>
2022-10-24 06:57:45 -0700

    Added task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - collect pointer and modify the value it is pointing to
+ * @n: pointer value
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
