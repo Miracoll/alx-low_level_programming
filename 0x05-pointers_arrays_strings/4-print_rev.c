04c59499e158b65ba10d71b79718d42776decda3
Miracle <obianomiracle@gmail.com>
2022-10-24 08:37:08 -0700

    Added task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,13 +10,13 @@ void print_rev(char *s)
 {
 	int count = 0;
 
-	while (s[i] != '\0')
+	while (s[count] != '\0')
 		count++;
 	count--;
 	while (count >= 0)
 	{
-		_putchar(s[i]);
-		count++;
+		_putchar(s[count]);
+		count--;
 	}
 	_putchar('\n');
 }
