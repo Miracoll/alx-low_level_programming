6c7bd9cfc79173e0ef499dc0294c5ef35218cb26
Miracle <obianomiracle@gmail.com>
2022-10-24 07:03:31 -0700

    Added task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int -  interchange both value
+ * @a: first value
+ * @b: second value
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	int change;
+	change = *a;
+	*a = *b;
+	*b = change;
+}
