912bbc19e0e9732ebbbfb2e48f3d6d8d1da13e40
Miracle <obianomiracle@gmail.com>
2022-10-24 09:36:51 -0700

    addition

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts_half - output half of the string
+ * @str: string
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int i = 0;
+	int j, max;
+
+	while (str[i] != '\0')
+		i++;
+	max = i;
+	j = max / 2;
+	while (j <= max)
+	{
+		_putchar(str[j]);
+		j++;
+	}
+	_putchar;
+}
